15eea73a6e9236412d451bfd1fb192ce36e96a7a
ltsy2010 <shiyul0101@gmail.com>
2021-05-09 18:18:37 -0700

    web style

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+//import logo from './logo.svg';
+//import '../styles/App.css';
+
+import Header from './Header';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
