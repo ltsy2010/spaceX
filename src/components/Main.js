bafc6e8ab5abb6feb17617a4fc42c3cfd1e62d68
ltsy2010 <shiyul0101@gmail.com>
2021-05-12 08:58:28 -0700

    web layout

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,20 @@
 import React, {Component} from 'react';
 import { Row, Col } from 'antd';
+import SatSetting from "./SatSetting";
+import SatelliteList from './SatelliteList';
 
 class Main extends Component {
     render() {
         return (
             <Row>
-                main
+                <Col span={8} >
+                    <SatSetting />
+                    <SatelliteList />
+                </Col>
+                <Col span={16} className="right-side">
+                    right
+                </Col>
+
             </Row>
         );
     }
---
+++ src/constants.js
@@ -0,0 +1,5 @@
+export const SAT_API_KEY = "VHC764-BMJHKN-95U5C6-4POD";
+
+export const STARLINK_CATEGORY = "52";
+
+export const NEARBY_SATELLITE = "rest/v1/satellite/above";
